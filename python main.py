ce81b4da3ed9ba18a02e7f7674c6528bea61ade7
AlexaGenotek <alexa@genotek.uk>
2024-08-04 16:03:43 +0400

    python main.py

---
+++ python main.py
@@ -0,0 +1,7 @@
+Follow the GUI prompts to upload your CAD file and specify optimization parameters.
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
